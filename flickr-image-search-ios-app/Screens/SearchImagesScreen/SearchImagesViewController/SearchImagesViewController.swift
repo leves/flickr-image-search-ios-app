d802ab1ce9b6ae896f675808b4c06adf6cf20136
Levente Bernath <leves@users.noreply.github.com>
2021-04-25 22:13:30 +0200

    Init SearchImagesViewController

---
+++ flickr-image-search-ios-app/Screens/SearchImagesScreen/SearchImagesViewController/SearchImagesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SearchImagesViewController.swift
+//  flickr-image-search-ios-app
+//
+//  Created by Levente Bernáth on 2021. 04. 25..
+//
+
+import UIKit
+
+// MARK: - SearchImagesViewController
+
+final class SearchImagesViewController: UIViewController {
+    
+}
