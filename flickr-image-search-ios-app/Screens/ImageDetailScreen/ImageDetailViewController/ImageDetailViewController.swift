cb6c01dd61162568b4781a188fe4ca6e13cf4ac3
Levente Bernath <leves@users.noreply.github.com>
2021-04-25 23:52:51 +0200

    Init ImageDetailViewController

---
+++ flickr-image-search-ios-app/Screens/ImageDetailScreen/ImageDetailViewController/ImageDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ImageDetailViewController.swift
+//  flickr-image-search-ios-app
+//
+//  Created by Levente Bernáth on 2021. 04. 25..
+//
+
+import UIKit
+
+// MARK: - ImageDetailViewController
+
+final class ImageDetailViewController: UIViewController {
+    
+}
