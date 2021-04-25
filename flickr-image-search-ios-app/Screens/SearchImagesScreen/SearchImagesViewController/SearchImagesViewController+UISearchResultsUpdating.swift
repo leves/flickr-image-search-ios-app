058acd194f66abeb4f47ff60987bde6f29da4e44
Levente Bernath <leves@users.noreply.github.com>
2021-04-25 22:41:33 +0200

    Add SearchController for SearchImagesViewController

---
+++ flickr-image-search-ios-app/Screens/SearchImagesScreen/SearchImagesViewController/SearchImagesViewController+UISearchResultsUpdating.swift
@@ -0,0 +1,28 @@
+//
+//  SearchImagesViewController+UISearchResultsUpdating.swift
+//  flickr-image-search-ios-app
+//
+//  Created by Levente Bernáth on 2021. 04. 25..
+//
+
+import UIKit
+
+// MARK: - SearchImagesViewController+UISearchResultsUpdating
+
+extension SearchImagesViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else { return }
+        print(text)
+    }
+}
+
+// MARK: - SearchImagesViewController+UISearchBarDelegate
+
+extension SearchImagesViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchText = searchBar.text else { return }
+        self.viewModel?.searchImagesByText(searchText)
+    }
+}
--- flickr-image-search-ios-app/Screens/SearchImagesScreen/SearchImagesViewController/SearchImagesViewController.swift
+++ flickr-image-search-ios-app/Screens/SearchImagesScreen/SearchImagesViewController/SearchImagesViewController.swift
@@ -11,6 +11,23 @@ import UIKit
 
 final class SearchImagesViewController: UIViewController {
     
+    // MARK: - LocalizedText
+    
+    enum LocalizedText {
+        static let searchPlaceholder = NSLocalizedString("Search for Images", comment: "")
+    }
+    
+    // MARK: - UI Elements
+    
+    private lazy var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = LocalizedText.searchPlaceholder
+        searchController.searchBar.delegate = self
+        return searchController
+    }()
+    
     // MARK: - Properties
     
     weak var coordinator: ISearchImagesCoordinator?
@@ -28,10 +45,33 @@ final class SearchImagesViewController: UIViewController {
         setupViewModelBinding()
     }
     
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupSearchController()
+        resumeWithPreviousSearch()
+    }
+    
+    // MARK: - Setup Search
+    
+    private func setupSearchController() {
+        self.navigationItem.searchController = searchController
+        self.definesPresentationContext = true
+        searchController.searchBar.text = viewModel?.searchTerm ?? ""
+    }
+    
     // MARK: - Setup (Delegate)Binding between view controller and view model
     
     private func setupViewModelBinding() {
         self.viewModel?.delegate = self
+    }
+    
+    // MARK: - Resume With Previous Search
+    
+    private func resumeWithPreviousSearch() {
+        guard let previousSearchTerm = self.viewModel?.searchTerm else { return }
+        self.viewModel?.searchImagesByText(previousSearchTerm)
     }
 }
 
