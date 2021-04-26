60dbff95c7546023e576f03331136d9b758e4a70
Levente Bernath <leves@users.noreply.github.com>
2021-04-26 22:42:01 +0200

    Add custom property wrapper for UserDefault

--- flickr-image-search-ios-app/Screens/Shared/Helpers.swift
+++ flickr-image-search-ios-app/Screens/Shared/Helpers.swift
@@ -13,3 +13,25 @@ extension String {
     
     static var empty: String { "" }
 }
+
+// MARK: - UserDefault Property Wrapper
+
+@propertyWrapper
+struct UserDefault<Value> {
+    
+    let key: String
+    let defaultValue: Value
+    var container: UserDefaults = .standard
+
+    var wrappedValue: Value {
+        
+        get {
+            return container.object(forKey: key) as? Value ?? defaultValue
+        }
+        
+        set {
+            container.set(newValue, forKey: key)
+            container.synchronize()
+        }
+    }
+}
